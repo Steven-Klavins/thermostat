bcecd38e6af8abe5fdbb91b5fa92af65583a7549
Steven-Klavins <stevenklavins94@gmail.com>
2020-04-16 17:14:31 +0100

    added API

--- jasmine-standalone-3.5.0/src/thermostat-controller.js
+++ jasmine-standalone-3.5.0/src/thermostat-controller.js
@@ -29,4 +29,34 @@ $( document ).ready(function() {
     $( "#current-temp" ).text(thermostat.temp);
     $( "#usage" ).text(thermostat.usage());
   }
+
+
+$.ajax({
+ 
+  // The URL for the request
+  url: "http://api.openweathermap.org/data/2.5/weather?q=London&appid=05b91837eefbb10db098afbbc580255e&units=metric",
+
+  // Whether this is a POST or GET request
+  type: "GET",
+
+  // The type of data we expect back
+  dataType : "json",
+})
+// Code to run if the request succeeds (is done);
+// The response is passed to the function
+.done(function( json ) {
+console.log (json.main.temp)
+$( "#weather" ).text(json.main.temp);
+})
+
+
+// Code to run if the request fails; the raw request and
+// status codes are passed to the function
+.fail(function( xhr, status, errorThrown ) {
+  alert( "Sorry, there was a problem!" );
+  console.log( "Error: " + errorThrown );
+  console.log( "Status: " + status );
+  console.dir( xhr );
+})
+
 });
