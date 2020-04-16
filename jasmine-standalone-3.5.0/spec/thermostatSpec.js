5cbc7f833387f4d5cb4d954aa5624b415a4f7d2c
Steven-Klavins <stevenklavins94@gmail.com>
2020-04-16 14:34:38 +0100

    added the power saving limits as constants

--- jasmine-standalone-3.5.0/spec/thermostatSpec.js
+++ jasmine-standalone-3.5.0/spec/thermostatSpec.js
@@ -20,15 +20,28 @@ describe('Thermostat', function() {
       expect(thermostat.temp).toEqual(18)
     })
 
-    it('temp can be decreased', function() {
+    it('temp wont fall below 10', function() {
       thermostat.decrease(11)
       expect(thermostat.temp).toEqual(10)
     })
 
-    it('temp can be decreased', function() {
+    it('limits the temp to 25', function() {
       thermostat.powerSavingOn()
       thermostat.increase(6)
       expect(thermostat.temp).toEqual(25)
+    })
+
+    it('switches power saving off', function() {
+      thermostat.powerSavingOff()
+      thermostat.increase(10)
+      expect(thermostat.temp).toEqual(30)
+    })
+
+
+    it('removes the limit of 25, limit is now 32', function() {
+      thermostat.powerSavingOff()
+      thermostat.increase(20)
+      expect(thermostat.temp).toEqual(32)
     })
 
 }); 
--- jasmine-standalone-3.5.0/src/thermostat.js
+++ jasmine-standalone-3.5.0/src/thermostat.js
@@ -1,22 +1,33 @@
 function Thermostat() {
-  this.temp = 20;
+  this.DEFAULT_TEMP = 20;
+  this.temp = this.DEFAULT_TEMP;
+  this.MAX_TEMP_ON = 25;
+  this.MAX_TEMP_OFF = 32;
+  this.MIN_TEMP = 10;
   this.savingOn = false;
 }
 
 Thermostat.prototype.increase = function(degrees) {
   this.temp += degrees
-  if (this.temp > 25 && this.savingOn == true) {
-    this.temp = 25;
+  if (this.temp > this.MAX_TEMP_ON && this.savingOn == true) {
+    this.temp = this.MAX_TEMP_ON;
+  }
+  if (this.temp > this.MAX_TEMP_OFF && this.savingOn == false){
+    this.temp = this.MAX_TEMP_OFF
   }
 }
 
 Thermostat.prototype.decrease = function(degrees) {
   this.temp -= degrees
-  if (this.temp < 10) {
-    this.temp = 10;
+  if (this.temp < this.MIN_TEMP) {
+    this.temp = this.MIN_TEMP;
   }
 }
 
 Thermostat.prototype.powerSavingOn = function() {
   this.savingOn = true;
+}
+
+Thermostat.prototype.powerSavingOff = function(){
+  this.savingOn = false
 }
